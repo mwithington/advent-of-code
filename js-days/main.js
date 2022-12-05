0a4b40c7abd8e619d22deef57373cb3e508063c2
Mike Withington <michael.withington@simplisafe.com>
2022-12-05 15:09:05 -0500

    first star get

--- js-days/main.js
+++ js-days/main.js
@@ -4,10 +4,33 @@ function countCalories() {
     
 }
 
-function splitElves() {
+function splitElves(elvesList) {
+    return elvesList.split('\n\n');
+}
 
+function caloriesPerElf(elvesArray) {
+    const elf = elvesArray.split('\n');
+    return  elf
+        .map( function(elt) {
+            return /^\d+$/.test(elt) ? parseInt(elt) : 0; 
+        })
+        .reduce( function(a,b) { 
+            return a+b
+        });
 }
 
 function run() {
-    
+   const input = fs.readFileSync('./input.txt');
+   console.log(input);
+   const separatedElves = splitElves(input.toString());
+   console.log(separatedElves);
+   let bigBoy = 0;
+   for (let index = 0; index < separatedElves.length-1; index++) {
+    const elf_1 = caloriesPerElf(separatedElves[index]);
+    const elf_2 = caloriesPerElf(separatedElves[index+1]);
+    bigBoy = Math.max(bigBoy, elf_1, elf_2);
+   }
+  console.log(bigBoy); 
 }
+
+run();
