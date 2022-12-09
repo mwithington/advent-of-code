9a6e0af0c4934985c1c7176b7faeee0de7d8baa5
Mike Withington <michael.withington@simplisafe.com>
2022-12-09 18:10:22 -0500

    working a little bit

--- 2022/js-days/9/main.js
+++ 2022/js-days/9/main.js
@@ -38,7 +38,39 @@ class Rope {
         this.tail = tail;
     }
     areAdj() {
+        const xDiff = this.head.x - this.tail.x;
+        const yDiff = this.head.y - this.tail.y;
+        // console.log(this.head.x, this.tail.x);
+        if(xDiff > 1 || xDiff < -1){
+            console.log('Needs x movement');
+            return false;
+        }
+        if (yDiff > 1 || yDiff < -1) {
+            console.log('Needs y movement'); 
+            return false;
+        }
+        return true;
+    }
+    areDiag() {
+        const xDiff = this.head.x - this.tail.x;
+        const yDiff = this.head.y - this.tail.y;
+        const upRight = xDiff == 1 && yDiff == 1;
+        const upLeft = xDiff == -1 && yDiff == 1; 
+        const downLeft = xDiff == -1 && yDiff == -1;
+        const downRight = xDiff == 1 && yDiff == -1;
 
+        if(upRight || upLeft || downRight || downLeft) {
+            console.log('needs diag move');
+            return true;   
+        } 
+        return false;
+    }
+
+    calcPosDiff(){
+        return {
+            xDiff: this.head.x - this.tail.x,
+            yDiff: this.head.y - this.tail.y 
+        }
     }
 }
 
@@ -54,24 +86,61 @@ function run() {
     const rope = new Rope(new Coord(0,0), new Coord(0, 0));
     const headMoves = split(input);
     const tailMoves = new Set();
-    console.log(headMoves);
+    tailMoves.add(`${rope.tail.x},${rope.tail.y}`); 
     for (let index = 0; index < headMoves.length; index++) {
         const move = headMoves[index].split(' ');
         console.log(move);
         const axis = MOVE_ENUM[move[0]].axis;
         const dirMod = MOVE_ENUM[move[0]].dir * move[1];
-        rope.head[axis] += dirMod;
+        // rope.head[axis] += dirMod;
         console.log(axis, dirMod);
-
-        if(!rope.areAdj()) {
-            // need to move the tail and track location
-            console.log('tail needs move too');
+        
+        let headMv = 0;
+        while(headMv != dirMod){
+            let mod = 1 * MOVE_ENUM[move[0]].dir;
+            console.log('move modifier', mod); 
+            if(!rope.areAdj()) {
+                rope.tail[axis] += mod;
+                tailMoves.add(`${rope.tail.x},${rope.tail.y}`);
+            } else {
+                rope.head[axis] += mod;
+                // check on final loop to see if caught up
+                if(!rope.areAdj()) {
+                    rope.tail[axis] += mod;
+                    tailMoves.add(`${rope.tail.x},${rope.tail.y}`);
+                }
+                if(rope.areDiag()) {
+                    const diff = rope.calcPosDiff();
+                    rope.tail.x += diff.xDiff;
+                    rope.tail.y += diff.yDiff;
+                    tailMoves.add(`${rope.tail.x},${rope.tail.y}`); 
+                }
+                headMv += mod;
+            } 
         }
-        
-        // break;
-        
+        // for (let j = 0; j < dirMod; j++) {
+        //     if(!rope.areAdj()) {
+        //         rope.tail[axis]++;
+        //         tailMoves.add(`${rope.tail.x},${rope.tail.y}`);
+        //     }
+        //     rope.head[axis]++;
+        //     // while(!rope.areAdj()) {
+        //     //     rope.tail[axis]++;
+        //     //     tailMoves.add(`${rope.tail.x},${rope.tail.y}`);   
+        //     // }
+            
+        // }
+        if(index == 3){
+            break;
+        }
+        // if(!rope.areAdj()) {
+        //     // need to move the tail and track location
+            
+        // }
+    
     }
     console.log(rope);
+    console.log(tailMoves);
 }
 
 run();
